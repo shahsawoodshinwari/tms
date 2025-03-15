1dd99ed4e9b558ff45b6e5895032fa4334775270
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-03-15 22:59:01 +0500

    chore: latest fixes

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Sync contents from ../tms-frontend/dist/ excluding .git and .github
+rsync -av --delete --exclude=.git --exclude=.github --exclude=sync.sh ../tms-frontend/dist ./
+
+# Copy index.html to 404.html
+cp index.html 404.html
