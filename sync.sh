27c90aaa5caadd5822dacc4a7e609472d283346f
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-03-15 23:05:37 +0500

    chore: fixed build

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sync contents from ../tms-frontend/dist/ excluding .git and .github
-rsync -av --delete --exclude=.git --exclude=.github --exclude=sync.sh ../tms-frontend/dist ./
+rsync -av --delete --exclude=.git --exclude=.github --exclude=sync.sh ../tms-frontend/dist/ ./
 
 # Copy index.html to 404.html
 cp index.html 404.html
